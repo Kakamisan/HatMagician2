4ef24690873f86cc66db0cf6b05dc5a7d0947f47
kakami <kakamisound@outlook.com>
2026-07-31 14:41:45 +0800

    110版本新增角色形态节点

--- HatMagician2Code/SceneControl/NMerchantCharacterSub.cs
+++ HatMagician2Code/SceneControl/NMerchantCharacterSub.cs
@@ -4,6 +4,4 @@ using MegaCrit.Sts2.Core.Nodes.Screens.Shops;
 namespace HatMagician2.HatMagician2Code.SceneControl;
 
 [GlobalClass]
-public partial class NMerchantCharacterSub : NMerchantCharacter
-{
-}
+public partial class NMerchantCharacterSub : NMerchantCharacter;
--- HatMagician2Code/SceneControl/NRestSiteCharacterChild.cs
+++ HatMagician2Code/SceneControl/NRestSiteCharacterChild.cs
@@ -4,6 +4,4 @@ using MegaCrit.Sts2.Core.Nodes.RestSite;
 namespace HatMagician2.HatMagician2Code.SceneControl;
 
 [GlobalClass]
-public partial class NRestSiteCharacterChild : NRestSiteCharacter
-{
-}
+public partial class NRestSiteCharacterChild : NRestSiteCharacter;
